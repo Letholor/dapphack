f66b12de8bd36af6d77e78b8d0ab50ff898694cc
DanielGale <daniel.gale@gmail.com>
2018-06-02 14:24:04 -0400

    add student data

---
+++ public/schoolData.js
@@ -0,0 +1,13 @@
+var schoolData = [
+    {
+        "class": "2017-2018",
+        "classList": [
+            {
+                "id": 1,
+                "firstName": "Peter",
+                "lastName": "Parker",
+                "grade": "A"
+            }
+        ]
+    }
+];
