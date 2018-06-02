6a90c6aa276200077c8998ba03c40470128705cb
DanielGale <daniel.gale@gmail.com>
2018-06-02 10:20:13 -0400

    Fix js logic For signout

--- public/app.js
+++ public/app.js
@@ -1,30 +1,40 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('teacherButton').addEventListener('click', function(event) {
-    event.preventDefault()
-    blockstack.redirectToSignIn()
-  })
-  document.getElementById('signout-button').addEventListener('click', function(event) {
-    event.preventDefault()
-    blockstack.signUserOut("http://localhost:5000")
-  })
+document.addEventListener("DOMContentLoaded", function (event) {
+  var teacherButton = document.getElementById('teacherButton');
+  if (teacherButton) {
+    teacherButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      blockstack.redirectToSignIn();
+      //window.location = "http://localhost:5000/teacher.html";
+    })
+  }
+  var signoutButton = document.getElementById("signout-button");
+  if (signoutButton) {
+    signoutButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      blockstack.signUserOut("http://localhost:5000/");
+    })
+  }
 
-  function showProfile(profile) {
+  /*function showProfile(profile) {
     var person = new blockstack.Person(profile)
     document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
-    if(person.avatarUrl()) {
+    if (person.avatarUrl()) {
       document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
     }
     document.getElementById('section-1').style.display = 'none'
     document.getElementById('section-2').style.display = 'block'
-  }
+  }*/
 
   if (blockstack.isUserSignedIn()) {
-    window.location = "http://localhost:5000/teacher.html" ;
-    var profile = blockstack.loadUserData().profile
-      showProfile(profile)
+    var profile = blockstack.loadUserData().profile;
+    var person = new blockstack.Person(profile);
+
+    document.getElementById('teacherName').innerHTML = person.name() ? person.name() : "Teacher";
+
+    //showProfile(profile)
   } else if (blockstack.isSignInPending()) {
-    blockstack.handlePendingSignIn().then(function(userData) {
-      window.location = window.location.origin;
-    })
+    blockstack.handlePendingSignIn().then(function (userData) {
+      window.location = "http://localhost:5000/teacher.html";
+    });
   }
-})
+});
