5e27fb8c70fbd28afe557e8bcf4ea1f440d45e41
Lee Reynolds <lee.reynolds2008@gmail.com>
2018-06-02 14:30:01 -0400

    added more data

--- public/schoolData.js
+++ public/schoolData.js
@@ -7,6 +7,61 @@ var schoolData = [
                 "firstName": "Peter",
                 "lastName": "Parker",
                 "grade": "A"
+            },{
+                "id": 2,
+                "firstName": "Xaden",
+                "lastName": "Lynser",
+                "grade": "A"
+            },{
+                "id": 3,
+                "firstName": "Rezon",
+                "lastName": "Welfore",
+                "grade": "A"
+            },{
+                "id": 4,
+                "firstName": "Ryushi",
+                "lastName": "Rayne",
+                "grade": "A"
+            },{
+                "id": 5,
+                "firstName": "Bruce",
+                "lastName": "Wayne",
+                "grade": "A"
+            },{
+                "id": 6,
+                "firstName": "Ariya",
+                "lastName": "Stanley",
+                "grade": "A"
+            },{
+                "id": 7,
+                "firstName": "Letho",
+                "lastName": "Richet",
+                "grade": "A"
+            },{
+                "id": 8,
+                "firstName": "Jogle",
+                "lastName": "Gail",
+                "grade": "A"
+            },{
+                "id": 9,
+                "firstName": "Pany",
+                "lastName": "Parker",
+                "grade": "A"
+            },{
+                "id": 10,
+                "firstName": "Harry",
+                "lastName": "Porter",
+                "grade": "A"
+            },{
+                "id": 11,
+                "firstName": "Noreason",
+                "lastName": "broker",
+                "grade": "A"
+            },{
+                "id": 12,
+                "firstName": "Fancy",
+                "lastName": "Hedgeworth",
+                "grade": "A"
             }
         ]
     }
